8fc3cc93d918c2053843c05f802b7c7f47d22430
FahmiDhika <fahmidhika810@gmail.com>
2024-11-20 18:25:36 +0700

    buat form login

---
+++ app/login/page.tsx
@@ -0,0 +1,49 @@
+import { LoginButton } from "@/components/button";
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <section className="flex flex-col justify-center items-center h-dvh">
+      <div className="bg-[#323232] text-white py-6 px-12 rounded-[20px] shadow-xl shadow-gray-600 mb-16">
+        <h1 className="font-bold text-[32px] text-center mb-9">SIGN IN</h1>
+
+        <div className="mb-6">
+          <label htmlFor="name" className="block mb-2 font-medium text-white">
+            Nama
+          </label>
+          <input
+            type="name"
+            name="name"
+            placeholder="admin123"
+            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg p-2.5"
+          />
+        </div>
+        <div className="mb-12">
+          <label
+            htmlFor="password"
+            className="block mb-2 font-medium text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="adminpass123"
+            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg p-2.5"
+          />
+        </div>
+
+        <LoginButton />
+      </div>
+
+      <Link href={"/"}>
+        <div className="mt-6 xl:mt-0 boton-elegante">
+            Halaman Utama
+        </div>
+      </Link>
+        
+    </section>
+  );
+};
+
+export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="flex flex-wrap justify-center items-center h-dvh ">
+    <section className="flex flex-wrap justify-center items-center h-dvh homepage">
       <div className="w-full text-center">
         <h1 className="text-4xl font-bold xl:text-6xl">Selamat Datang.</h1>
         <h2 className="text-xl xl:text-2xl">Semangat bekerja</h2>
       </div>
 
       <Link href={"login"}>
-        <div className="w-auto text-center py-4 px-8 rounded-xl text-2xl bg-[#323232] text-white">
+        <div className="boton-elegante">
           Login
         </div>
       </Link>
